84cb9951808e287351d91ce76098c4159b1364be
Tam Nguyen <nttamspkt@gmail.com>
2022-07-21 10:20:14 +0700

    Init project

---
+++ main.js
@@ -0,0 +1,10 @@
+/**
+ * Bài 1: Cho nguời dùng nhập 3 số nguyên. Viết chương trình xuất 3 số theo thứ
+        tự tăng dần
+ */
+document.getElementById("btnAscending").onclick = function () {
+  // input
+  var num1 = document.getElementById("num1").value * 1;
+  var num2 = document.getElementById("num2").value * 1;
+  var num3 = document.getElementById("num3").value * 1;
+};
